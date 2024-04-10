38e77e33a684efd055a3d0b673e2f846099b17c7
Juan Altmayer Pizzorno <juan@altmayer.com>
2024-04-10 12:21:38 -0400

    - fixed bug parsing options; - added support for --isolate-tests;

--- replication/eval_codamosa.py
+++ replication/eval_codamosa.py
@@ -9,20 +9,28 @@ import sys
 
 def parse_args():
     import argparse
+    ap = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
     ap.add_argument('--codamosa-results', choices=['gpt4', 'codex'], default='gpt4',
                     help='codamosa results to use')
+    ap.add_argument('--isolate-tests', default=False,
+                    action=argparse.BooleanOptionalAction,
+                    help='run tests in isolation (to work around any state pollution) when measuring coverage')
+
     return ap.parse_args()
 
 args = parse_args()
 
 codamosa_tests = Path("gpt4-coda") if args.codamosa_results == 'gpt4' else \
                  Path("codamosa-dataset/final-exp/codamosa-0.8-uninterp")
-output = Path(f"output-{args.codamosa_results}")
+output = Path(f"output-{args.codamosa_results}" + ("-isolated" if args.isolate_tests else ""))
 
 modules_csv = Path("test-apps") / "good_modules.csv"
 
 
 output.mkdir(exist_ok=True)
+
+slipcover_args = '--isolate-tests' if args.isolate_tests else ''
 
 with modules_csv.open() as f:
     reader = csv.reader(f)
@@ -35,14 +43,14 @@ with modules_csv.open() as f:
 
             pkg = module.split('.')[0]
 
-            opts = ''
+            pytest_args = ''
 
             # Some of these 'failing' tests damage the system so much that it is not
             # possible to get a coverage measurement.  Disabling them turned out to be
             # unnecessary because some of the generations (samples) for flutils don't
             # have this issue.
 #            if output.name == 'output-codex' and module == 'flutils.pathutils':
-#                opts += f'--ignore=/tests/test_{"_".join(module.split("."))}_failing.py'
+#                pytest_args += f'--ignore=/tests/test_{"_".join(module.split("."))}_failing.py'
 
             print(f"---- {datetime.now().isoformat()} {str(coda_run)} ----")
 
@@ -51,8 +59,9 @@ with modules_csv.open() as f:
                   f"-v {str(coda_run.absolute())}:/tests:ro " +\
                   f"-v {str(output.absolute())}:/output " +\
                   f"-v ./{p}:/package:ro " +\
+                  f"-v {str((Path('.') / 'pip-cache').absolute())}:/root/.cache/pip " +\
                    "-t slipcover-runner " +\
-                  f"bash /run_coda_tests.sh {pkg} {json.name} \"{opts}\""
+                  f"bash /run_coda_tests.sh {pkg} {json.name} \"{slipcover_args}\" \"{pytest_args}\""
             print(cmd)
             subprocess.run(cmd, shell=True, check=False)
 #            sys.exit(0)
--- replication/run_coda_tests.sh
+++ replication/run_coda_tests.sh
@@ -8,13 +8,15 @@ cd package
 # install dependencies
 pip install -r package.txt #|| exit 1
 
-# install timeout plugin
-pip install pytest-timeout
+# no longer needed: 'slipcover-runner' image now has these
+#pip install --upgrade 'slipcover==1.0.6' || exit 1
+#pip install pytest-timeout pytest-forked || exit 1
 
 PKG=$1
 JSON=$2
-OPTS=$3
+SLIPCOVER_ARGS=$3
+PYTEST_ARGS=$4
 
-CMD="python3 -m slipcover --source $PKG --branch --json --out /output/$JSON -m pytest --rootdir . -c /dev/null -p no:cacheprovider --timeout 120 $OPTS /tests"
+CMD="python3 -m slipcover --source $PKG --branch --json --out /output/$JSON $SLIPCOVER_ARGS -m pytest --rootdir . -c /dev/null -p no:cacheprovider --timeout 120 $PYTEST_ARGS /tests"
 echo $CMD
 eval $CMD
